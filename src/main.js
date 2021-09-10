261a8a6d1026d35efa36e728aaa8f21d57f232a3
Kaiz <26181183+kaiz16@users.noreply.github.com>
2021-09-10 23:26:15 +0800

    some cmnt

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import { apolloClient } from "./vue-apollo";
+// some comment
 createApp(App)
   .provide(DefaultApolloClient, apolloClient)
   .mount("#app");
